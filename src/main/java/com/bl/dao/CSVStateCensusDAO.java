4e654106fbb8c5a60378476fbe11c179333a980c
priyankagarad <garadpriyanka1995@gmail.com>
2020-05-06 12:07:30 +0530

    ADDED:Use Stram Instead Of While loop For Loading CSV Data

--- src/main/java/com/bl/dao/CSVStateCensusDAO.java
+++ src/main/java/com/bl/dao/CSVStateCensusDAO.java
@@ -27,9 +27,6 @@ public class CSVStateCensusDAO<E>
         this.TIN = indiaStateCodeCSV.getTIN();
     }
 
-    public CSVStateCensusDAO(Class<E> csvClass) {
-    }
-
     public String getSrNo() {
         return SrNo;
     }
